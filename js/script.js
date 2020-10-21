162edc5878b089996b04b35e219c8ef3a9c5fd8a
Lina <car.badillom@gmail.com>
2020-10-20 22:22:54 -0500

    CSS, Canvas, html

--- js/script.js
+++ js/script.js
@@ -1,10 +1,41 @@
 // 1.Canvas
-//imgscale:1 si quieres modificar la imagen
+const img = new Image();
+img.src ="../Images/bg.png";
 
-//ctx.drawImage("IMAGE",coordenada x,coordenaday, Coordenada Canvas)
-draw(){
-    ctx.drawImage(imagen, this.x, this.y, this.weight,this.height);
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+const backgroundImage = {
+  img: img,
+  x: 0,
+  speed: -1,
+
+  move: function() {
+    this.x += this.speed;
+    this.x %= canvas.width;
+  },
+
+  draw: function() {
+    ctx.drawImage(this.img, this.x, 0);
+    if (this.speed < 0) {
+      ctx.drawImage(this.img, this.x + canvas.width, 0);
+    } else {
+      ctx.drawImage(this.img, this.x - this.img.width, 0);
     }
+  },
+};
+
+function updateCanvas() {
+  backgroundImage.move();
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  backgroundImage.draw();
+
+  requestAnimationFrame(updateCanvas);
+}
+
+// start calling updateCanvas once the image is loaded
+img.onload = updateCanvas;
 
 // Instancia "Pucca"
 let playerImg= new Image ();
@@ -26,7 +57,10 @@ width:50,
 height:50,
 speedX:3,
 lifes:5,
+moveUp:    function() { this.y -= 50 },
+moveDown:  function() { this.y += 50 },
 }
+
  /* Falta establecer movimiento en player*/
 const monster={
     x:25,
@@ -36,10 +70,10 @@ const monster={
     };
 
 const cat ={
-    x: 150
-    y:"";
+    x: 150,
+    y:"",
     width:50,
-    speed:random();
+    speed:random(),
     /* Falta establecer función random, donde los 
     gatos aparezcan de forma aleatoria a velocidades
     distintas*/
@@ -52,17 +86,14 @@ window.onload = function() {
     };
   };
   
-//5.Actualización de objetos
-const update = function (modifier) {
-	if (38 in keysDown) { // Arriba
-		player.y -= player.speed * modifier;
-	}
-	if (40 in keysDown) { // Abajo
-		player.y += player.speed * modifier;
-	}
-	if (39 in keysDown) { // Derecha
-		player.x += player.speed * modifier;
-	}
+/*5.Actualización de objetos
+document.onkeydown = function(e) {
+  switch (e.keyCode) {
+    case 38: player.moveUp();    console.log('up',    player); break;
+    case 40: player.moveDown();  console.log('down',  player); break;
+  }
+  updateCanvas();
+}
 //Colisión
     if (
 		player.x <= (monster.x + 50)
@@ -82,4 +113,4 @@ const update = function (modifier) {
     }
 };
 
-// INPUT (Keys)
+// INPUT (Keys)*/
